406160ddd0e9983933f9a56fa2149d23574992eb
sticx <156357861+pancachogod@users.noreply.github.com>
2025-10-19 13:55:14 -0500

    Update vite.config.js

--- tiktok-auction-top3/overlay/vite.config.js
+++ tiktok-auction-top3/overlay/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './',
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    host: true, // permite host externo
+    port: 8080, // necesario para Railway (usa $PORT)
+    allowedHosts: [
+      'keen-optimism-production.up.railway.app', // dominio de Railway (frontend)
+      'tiklive-production.up.railway.app'        // opcional si frontend y backend comparten host
+    ]
+  }
 })
